ec3441406db65a5860e2f463833cb734d050292f
Stephen Greene <sgreene@redhat.com>
2020-10-14 14:09:19 -0400

    cmd: Add canary image arg to CLI args
    Add the ingress canary image argument to the operator CLI startup code,
    and include a canary image variable in hack/run-local.sh. This requires
    the addition of the `CanaryImage` field in pkg/operator/config/config.go.

--- hack/run-local.sh
+++ hack/run-local.sh
@@ -6,6 +6,7 @@ oc scale --replicas 0 -n openshift-cluster-version deployments/cluster-version-o
 oc scale --replicas 0 -n openshift-ingress-operator deployments ingress-operator
 
 IMAGE=$(oc get -n openshift-ingress-operator deployments/ingress-operator -o json | jq -r '.spec.template.spec.containers[0].env[] | select(.name=="IMAGE").value')
+CANARY_IMAGE=$(oc get -n openshift-ingress-operator deployments/ingress-operator -o json | jq -r '.spec.template.spec.containers[0].env[] | select(.name=="CANARY_IMAGE").value')
 RELEASE_VERSION=$(oc get clusterversion/version -o json | jq -r '.status.desired.version')
 NAMESPACE="${NAMESPACE:-"openshift-ingress-operator"}"
 SHUTDOWN_FILE="${SHUTDOWN_FILE:-""}"
@@ -14,5 +15,5 @@ echo "Image: ${IMAGE}"
 echo "Release version: ${RELEASE_VERSION}"
 echo "Namespace: ${NAMESPACE}"
 
-${DELVE:-} ./ingress-operator start --image "${IMAGE}" --release-version "${RELEASE_VERSION}" \
+${DELVE:-} ./ingress-operator start --image "${IMAGE}" --canary-image "${CANARY_IMAGE}" --release-version "${RELEASE_VERSION}" \
 --namespace "${NAMESPACE}" --shutdown-file "${SHUTDOWN_FILE}" "$@"
