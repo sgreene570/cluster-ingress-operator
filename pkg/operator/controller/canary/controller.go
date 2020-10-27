59f90ddb39e8eaefeb2b7d0deae1612f052f8099
Stephen Greene <sgreene@redhat.com>
2020-10-27 16:25:25 -0400

    Canary controller phase 2
    Add basic HTTP probes and metrics.

--- pkg/operator/controller/canary/controller.go
+++ pkg/operator/controller/canary/controller.go
@@ -1,13 +1,21 @@
 package canary
 
 import (
+	"crypto/tls"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"net"
+	"net/http"
+	"strings"
+	"time"
 
 	logf "github.com/openshift/cluster-ingress-operator/pkg/log"
 	"github.com/openshift/cluster-ingress-operator/pkg/manifests"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/tcnksm/go-httpstat"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
 	routev1 "github.com/openshift/api/route/v1"
@@ -18,6 +26,8 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	utilnet "k8s.io/apimachinery/pkg/util/net"
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -63,6 +73,9 @@ func New(mgr manager.Manager, config Config) (controller.Controller, error) {
 	if err := c.Watch(&source.Kind{Type: &routev1.Route{}}, enqueueRequestForIngressCanary(manifests.DefaultCanaryNamespace)); err != nil {
 		return nil, err
 	}
+
+	// Start canary route polling loop
+	reconciler.startCanaryRoutePolling(config.Stop)
 	return c, nil
 }
 
@@ -169,6 +182,7 @@ func enqueueRequestForDefaultIngressController(namespace string) handler.EventHa
 type Config struct {
 	Namespace   string
 	CanaryImage string
+	Stop        chan struct{}
 }
 
 // reconciler handles the actual canary reconciliation logic in response to
@@ -180,11 +194,55 @@ type reconciler struct {
 	cache  cache.Cache
 }
 
+func (r *reconciler) startCanaryRoutePolling(stop <-chan struct{}) error {
+	//TODO
+	// check ingress controller status before starting polling loop?
+	count := 0
+	go wait.Until(func() {
+		haveRoute, route, err := r.currentCanaryRoute()
+		if err != nil || !haveRoute {
+			log.Error(err, "failed to get canary route")
+			return
+		}
+
+		// Periodically rotate route endpoint every 5 minutes
+		if count == 6 {
+			haveService, service, err := r.currentCanaryService()
+			if err != nil || !haveService {
+				log.Error(err, "failed to get canary service")
+				return
+			}
+			route, err = r.rotateRouteEndpoint(service, route)
+			if err != nil {
+				log.Error(err, "failed to rotate canary route endpoint")
+				return
+			}
+			log.Info("Rotate route endpoint, now on", "port", route.Spec.Port.TargetPort.String())
+			count = 0
+			// Give router time to reload
+			return
+		}
+
+		success, err := testRouteEndpoint(route)
+		host := route.Spec.Host
+		if !success {
+			log.Error(err, "canary route check:")
+			SetCanaryRouteUnreachable(host)
+			return
+		} else {
+			log.Info("Successful canary check?")
+			SetCanaryRouteReachable(host)
+			count++
+		}
+	}, 1*time.Minute, stop)
+
+	return nil
+}
+
 // Switch the current RoutePort that the route points to.
 // Use this function to periodically update the canary route endpoint
 // to verify if the router has wedged.
 func (r *reconciler) rotateRouteEndpoint(service *corev1.Service, current *routev1.Route) (*routev1.Route, error) {
-
 	updated, err := chooseRandomServicePort(service, current)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to rotate route port: %v", err)
@@ -228,4 +286,132 @@ func chooseRandomServicePort(service *corev1.Service, route *routev1.Route) (*ro
 	}
 
 	return updated, nil
+}
+
+// createRequest forms a url by combining the prefix (ie http://) and
+// host (ie <route>-<service>.apps.<cluster-domain>), and calls
+// http.NewRequest with the created url.
+func createRequest(host, prefix string) (*http.Request, error) {
+	url := prefix + host
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+// newHTTPClient creates an http Client with the specified
+// timeout and the default transport.
+func newHTTPClient(timeout time.Duration) *http.Client {
+	return &http.Client{Timeout: timeout}
+}
+
+// newHTTPSClient creates an http Client with the specified
+// timeout, and an http Transport created from config.
+func newHTTPSClient(timeout time.Duration, config *tls.Config) *http.Client {
+	client := newHTTPClient(timeout)
+	client.Transport = utilnet.SetTransportDefaults(&http.Transport{TLSClientConfig: config, DisableKeepAlives: true})
+	return client
+}
+
+// testRouteEndpoint probes the given route's host
+// and returns a bool indicating whether or not the request was
+// successful, along with an err if applicable.
+func testRouteEndpoint(route *routev1.Route) (bool, error) {
+	host := route.Spec.Host
+
+	if len(host) == 0 {
+		return false, fmt.Errorf("route.Spec.Host is nil, cannot test route")
+	}
+
+	// Create HTTP request
+	request, err := createRequest(host, "http://")
+	if err != nil {
+		return false, fmt.Errorf("Error creating canary HTTP request: %v", err)
+	}
+
+	// Create HTTP result
+	// for request stats tracking.
+	result := &httpstat.Result{}
+
+	// Get request context
+	ctx := httpstat.WithHTTPStat(request.Context(), result)
+	request = request.WithContext(ctx)
+
+	// Send the HTTP request
+	timeout, _ := time.ParseDuration("10s")
+	client := newHTTPClient(timeout)
+	response, err := client.Do(request)
+
+	if err != nil {
+		// Check if there were DNS errors
+		dnsErr := &net.DNSError{}
+		if errors.As(err, &dnsErr) {
+			// Handle DNS error
+			return false, fmt.Errorf("Error sending canary HTTP request: DNS error: %v", err)
+		}
+		return false, fmt.Errorf("Error sending canary HTTP request on host %s: %v", host, err)
+	}
+
+	// Read body and record current time
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return false, fmt.Errorf("Error reading canary response body: %v", err)
+	}
+	body := string(bodyBytes)
+	response.Body.Close()
+	t := time.Now()
+	// Mark request as finished
+	result.End(t)
+	totalTime := result.Total(t)
+
+	// log.Info("Total canary request time milliseconds", "time", totalTime.Milliseconds())
+
+	/**
+	// Example timing info
+	dnsTime := result.DNSLookup.Milliseconds()
+	tcpTime := result.TCPConnection.Milliseconds()
+	tlsTime := result.TLSHandshake.Milliseconds()
+	serverProcessingTime := result.ServerProcessing.Milliseconds()
+	contentTransferTime := result.ContentTransfer(t)
+	*/
+
+	// Verify body contents
+	if len(body) == 0 {
+		return false, fmt.Errorf("Expected canary response body to not be nil")
+	}
+
+	expectedBodyContents := "Hello OpenShift!"
+	if !strings.Contains(body, expectedBodyContents) {
+		return false, fmt.Errorf("Expected canary request body to contain %s, instead got %s", expectedBodyContents, body)
+	}
+
+	// Verify that the request was received on the correct port
+	recPort := response.Header.Get("request-port")
+	if len(recPort) == 0 {
+		return false, fmt.Errorf("Expected 'request-port' header in canary response to have a non-nil value")
+
+	}
+	routePortStr := route.Spec.Port.TargetPort.String()
+	if !strings.Contains(routePortStr, recPort) {
+		// router wedged, register in metrics counter
+		CanaryEndpointWrongPortEcho.Inc()
+		return false, fmt.Errorf("Canary request received on port %s, but route specifies %v", recPort, routePortStr)
+	}
+
+	// Check status code
+	status := response.StatusCode
+	switch status {
+	case 200:
+		// Register total time in metrics (use milliseconds)
+		CanaryRequestTime.WithLabelValues(host).Observe(float64(totalTime.Milliseconds()))
+	case 408:
+		return false, fmt.Errorf("Status code %d: request timed out", status)
+	case 503:
+		return false, fmt.Errorf("Status code %d: Route not available via router", status)
+	default:
+		return false, fmt.Errorf("Unexpected status code: %d", status)
+	}
+
+	return true, nil
 }
